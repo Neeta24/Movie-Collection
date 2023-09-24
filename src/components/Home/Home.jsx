88b35e8b617a543ef7e57df5243fb5a3b7817453
Neeta24 <neeta.sinha.24@gmail.com>
2023-09-24 18:12:20 +0600

    added home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Row from '../Row/Row';
 import requests from '../../Requests';
 import Main from '../Main/Main';
+import './Home.css'
 
 
 
@@ -11,7 +12,20 @@ const Home = () => {
         <div>
             <Main></Main>
             <Row title='MOVIES YOU MUST WATCH' fetchURL ={requests.popular}></Row>
-            <Row title='RECOMENDED FOR YOU' fetchURL ={requests.recomended}></Row>
+            <div className='heading'>
+                <h4>RECOMENDED FOR YOU</h4>
+            </div>
+            <div>
+                <button className='button-details red-color'>Hindi</button>
+                <button className='button-details ass-color'>Marathi</button>
+                <button className='button-details ass-color'>Bengali</button>
+                <button className='button-details ass-color'>Assamese</button>
+                <button className='button-details ass-color'>Telugu</button>
+                <button className='button-details red-color'>Tamil</button>
+                <button className='button-details ass-color'>Malayalam</button>
+                
+            </div>
+            <Row  fetchURL ={requests.recomended}></Row>
             <Row title='HOLLYWOOD CLASSICS' fetchURL ={requests.classic}></Row>
          
         </div>
